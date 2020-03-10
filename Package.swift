c5fb31a305514f3fa635fc8e55e25f5383ae2970
Rob Caraway <robbor911@gmail.com>
2020-03-09 21:17:26 -0500

    changed naming

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "BasicDefaults",
+    name: "HappyDefaults",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "BasicDefaults",
-            targets: ["BasicDefaults"]),
+            name: "HappyDefaults",
+            targets: ["HappyDefaults"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "BasicDefaults",
+            name: "HappyDefaults",
             dependencies: []),
         .testTarget(
             name: "BasicDefaultsTests",
-            dependencies: ["BasicDefaults"]),
+            dependencies: ["HappyDefaults"]),
     ]
 )
--- Tests/BasicDefaultsTests/BasicDefaultsTests.swift
+++ Tests/BasicDefaultsTests/BasicDefaultsTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import BasicDefaults
+@testable import HappyDefaults
 
 final class BasicDefaultsTests: XCTestCase {
     func testExample() {
